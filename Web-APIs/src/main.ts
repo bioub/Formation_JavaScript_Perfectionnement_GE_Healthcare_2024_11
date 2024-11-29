ea8f6fe5997f2277a51e45588f85d2b22c4572fb
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2024-11-29 11:08:31 +0100

    Enoncé

--- Web-APIs/src/main.ts
+++ Web-APIs/src/main.ts
@@ -11,6 +11,14 @@ const listEl = document.querySelector('.todo-list') as HTMLDivElement;
 formEl.addEventListener('submit', (event) => {
   event.preventDefault();
 
+  // Exercice 6
+  // A l'ajout d'une Todo
+  // En utilisant une expression régulière (voir les slides)
+  // Interdire les caractères :
+  // % @ et les chiffres dans le champ (ne rien faire si ces chars sont présent)
+  // Bonus : Afficher une erreur dans un div si les caractères sont présent
+  // Bonus : Afficher le champs en rouge si une erreur se produit (CSSOM)
+
   const divEl = createTodoItem({
     id: Math.random(),
     title: inputEl.value,
@@ -32,3 +40,14 @@ togglerEl.addEventListener('click', () => {
     checkboxEl.checked = togglerEl.checked;
   }
 });
+
+// Exercice 7
+// En utilisant la technique "Event Delegation",
+// Ramener les 3 addEventListener de todos.js ici
+// en écoutant sur des éléments qui existent au chargement
+// (exemple listEl)
+// Indication : les variables divEl, spanEl... n'existent pas ici
+// parcourir l'arbre pour retrouver les éléments correspondant
+// (en partant de event.target ou event.currentTarget)
+// Indication en TS:
+// const target = event.target as HTMLElement;
